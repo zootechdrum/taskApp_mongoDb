71e69c8f01eab434f47c077cd0b8aa01ee90c6e4
Cesar Gomez <zootechdrum@gmail.com>
2019-11-30 10:11:09 -0800

    added documents to db

--- mongodb.js
+++ mongodb.js
@@ -3,6 +3,7 @@ const mongodb = require('mongodb')
 //Initialize connection gives access to function necessary to create CRUD operations
 const MongoClient = mongodb.MongoClient;
 
+
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
@@ -10,5 +11,12 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     if(error){
         return console.log("Unable to connect to Database")
     }
-    console.log("Connected Correctly")
+
+    //Gets refernce to the specific db to manipulate
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Cesar',
+        age: '29'
+    })
 })
