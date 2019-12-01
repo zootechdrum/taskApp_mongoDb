cfdc8169e59fbcbaab09ae5fb881a2fcaa75a74a
Cesar Gomez <zootechdrum@gmail.com>
2019-12-01 11:26:20 -0800

    added rout paths for getting all tasks and individual tasks

--- src/index.js
+++ src/index.js
@@ -12,18 +12,60 @@ const port = process.env.PORT || 3000;
 app.use(express.json())
 
 app.post('/users', (req, res) => {
-    const user = new User (req.body)
+    const user = new User(req.body)
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
     })
 });
 
-app.post('/task', (req, res) => {
-    const task = new Task (req.body)
-    task.save().then(() => {
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch(() => {
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    });
+})
+
+app.get('/task', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+app.get('/task/:id', (req, res) => {
+    const task_id = req.params.id
+
+    Task.findById(task_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
         res.send(task)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.post('/task', (req, res) => {
+    const task = new Task(req.body)
+    task.save().then(() => {
+        res.status(201).send(task)
     }).catch((e) => {
         res.status(400).send(e)
     })
