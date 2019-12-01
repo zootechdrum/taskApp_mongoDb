b49e719e6132e37a5c4262d048d908246e597869
Cesar Gomez <zootechdrum@gmail.com>
2019-12-01 10:57:47 -0800

    changed the directory of task model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,16 +7,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-const Task = mongoose.model("Task", {
-    description: {
-        type: String,
-        required : true,
-        trime: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
+
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 
@@ -17,7 +18,18 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e)
     })
-})
+});
+
+app.post('/task', (req, res) => {
+    const task = new Task (req.body)
+    task.save().then(() => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+});
+
+
 
 app.listen(port, () => {
     console.log("Server is up and running on port" + port)
