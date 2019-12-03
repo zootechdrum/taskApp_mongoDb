9addadd704011bef138bc6c252f85d558d48c23e
Cesar Gomez <zootechdrum@gmail.com>
2019-12-02 18:30:12 -0800

    added upate method to update user

--- src/index.js
+++ src/index.js
@@ -45,6 +45,18 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/users/id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true , runValidators: true})
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
+
 app.get('/task', async (req, res) => {
 
     try {
@@ -78,18 +90,6 @@ app.post('/task', async (req, res) => {
         res.status(404).send(e)
     }
 });
-
-// app.post('/users', async (req, res) => {
-//     const user = new User(req.body)
-
-//     try {
-//         await user.save()
-//         res.status(201).send(user)
-//     } catch (e) {
-//         res.status(404).send(e)
-//     }
-// });
-
 
 app.listen(port, () => {
     console.log("Server is up and running on port" + port)
