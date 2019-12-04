5f410afd65370a58a4b6929d27b0874f06f59fc0
Cesar Gomez <zootechdrum@gmail.com>
2019-12-03 20:43:03 -0800

    added error message incase  user updates property not valid in our model

--- src/index.js
+++ src/index.js
@@ -47,6 +47,16 @@ app.get('/users/:id', async (req, res) => {
 })
 
 app.patch('/users/:id', async (req, res) => {
+    //makes sure we are only upating properties in our user model.
+
+    const update = Object.keys(req.body)
+    const allowedUpdates = ["name", "email", "password", "age"]
+    const isValidOperation = update.every((update) => allowedUpdates.includes(update))
+
+    if(!isValidOperation) {
+        return res.status(400)({error: "invalid updates"})
+    }
+
     try {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true , runValidators: true})
         if (!user) {
